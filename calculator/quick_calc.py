2f83bd9f57b7aff30d8307e63c04f49737219b20
ms-anugraheetha <anugraheetha.bs@gmail.com>
2026-02-25 11:48:55 +0200

    feat: add calculator core logic

---
+++ calculator/quick_calc.py
@@ -0,0 +1,22 @@
+class QuickCalc:
+
+    def __init__(self):
+        self.result = 0
+
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
+
+    def multiply(self, a, b):
+        return a * b
+
+    def divide(self, a, b):
+        if b == 0:
+            raise ValueError("Cannot divide by zero")
+        return a / b
+
+    def clear(self):
+        self.result = 0
+        return self.result
